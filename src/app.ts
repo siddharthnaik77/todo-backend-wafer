1370e297f38fa65e676a48aa5df460bb6b154c9f
Siddharth Naik <your.email@example.com>
2026-02-14 19:06:00 +0530

    added chnges

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,12 @@ import sequelize from "./config/db";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://todo-frontend-wafer.vercel.app",
+  })
+);
+// app.use(cors());
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
