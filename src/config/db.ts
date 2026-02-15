e958dfdea6e8bbe9e519aca9965400b0227290d4
Siddharth Naik <your.email@example.com>
2026-02-15 12:53:50 +0530

    vercel configuration changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,21 @@
-import { Sequelize } from "sequelize"
-require('dotenv').config()
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "todo_db",
-  process.env.DB_USER || "root",
-  process.env.DB_PASS || "",
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const useDb = process.env.USE_DB === "true";
+
+let sequelize: Sequelize | null = null;
+
+if (useDb) {
+  sequelize = new Sequelize(
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASS!,
+    {
+      host: process.env.DB_HOST!,
+      dialect: "mysql",
+    }
+  );
+}
 
 export default sequelize;
