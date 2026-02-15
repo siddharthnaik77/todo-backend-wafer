db4a61525a5e475e8e94701f1d4f5c8a7035deb8
Siddharth Naik <your.email@example.com>
2026-02-15 12:39:20 +0530

    vercel configuration changes

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
-import app from "../app";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import app from "../src/app";
 
-export default app;
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  app(req, res); 
+}
